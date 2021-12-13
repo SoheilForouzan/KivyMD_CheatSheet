4f7f989c3ae589a11fcd5bcc4b20369af1587f60
SoheilForouzan <soheil.forouzan@protonmail.com>
2021-12-13 05:34:07 +0330

    advanced swiper added

---
+++ swiper/with buttons/swiper.py
@@ -0,0 +1,14 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+
+class MainApp(MDApp):
+    def build(self):
+        # Makes the overall app theme Dark(default = Light)
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Blue"  # To set the primary pallet color
+        # To set the primary accent color. Visit https://github.com/kivymd/KivyMD/blob/master/kivymd/icon_definitions.py for more icons
+        return Builder.load_file('swiper.kv')
+
+
+MainApp().run()
